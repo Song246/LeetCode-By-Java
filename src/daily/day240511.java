bd756da068f4c47ff9996a72eda75ebf9a17f8e7
tckry <1150590475@qq.com>
2024-05-11 13:20:21 +0800

    2024/5/11 共享法

--- src/daily/day240511.java
+++ src/daily/day240511.java
@@ -1,5 +1,7 @@
 package daily;
 
+import java.util.HashSet;
+
 /**
  * 2391. 收集垃圾的最少总时间（模拟：分别计算收集垃圾时间和达到最后出现位置的路程时间，lastIdx）
  * @program: LeetCode
@@ -35,6 +37,23 @@ public class day240511 {
                     ans += travel[i];
             }
 
+            return ans;
+        }
+    }
+
+    // 贡献法：如果garbage[n−2]+garbage[n−1] 包含 M 和 P，
+    // 这意味着有两辆车需要行驶 travel[n−3] 分钟，所以travel[n−3] 对答案的贡献是 travel[n−3]⋅2。
+    class Solution2 {
+        public int garbageCollection(String[] garbage, int[] travel) {
+            int ans = garbage[0].length();
+            HashSet<Character> seen = new HashSet<>();
+            for (int i = garbage.length - 1; i > 0; i--) {
+                char[] g = garbage[i].toCharArray();
+                for (char c : g) {
+                    seen.add(c);
+                }
+                ans += g.length + travel[i - 1] * seen.size();
+            }
             return ans;
         }
     }
